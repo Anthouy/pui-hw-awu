52cb6b0d9098cda64547e166cfdbe8cb083873ae
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-09-23 15:35:36 -0400

    teset

--- solution-hw3/pricecomp.js
+++ solution-hw3/pricecomp.js
@@ -29,31 +29,5 @@ let allCinnamon = [
     },
 ]
 
-for (var key in fruits) {
-    var fruit = fruits[key];
-    _select.options[_select.options.length] = new Option(fruit.Description, fruit.id);
-  }
-  
-  document.body.appendChild(_select);
-
-
-  var fruits =
-{
-  "1": {
-        "id": 1,
-        "Description": "Apple",
-        "groupID": 0
-       },
-  "2": {
-        "id": 2,
-        "Description": "Peach",
-        "groupID": 0
-       }
-};
-var _select = document.createElement("select");
-for (var key in fruits) {
-  var fruit = fruits[key];
-  _select.options[_select.options.length] = new Option(fruit.Description, fruit.id);
-}
-
-document.body.appendChild(_select);
+let select = document.getElementById("gzing");
+select.options[select.options.length] = new Option('Text 1', 'Value1');

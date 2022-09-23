c7acb93cdb3cb87f380bc78cc5ee1bbcc177e7f8
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-09-23 15:54:19 -0400

    test

--- solution-hw3/pricecomp.js
+++ solution-hw3/pricecomp.js
@@ -2,14 +2,13 @@ console.log("Check1");
 
 let select = document.getElementById("glazing")
 
-let allCinnamon = [
+let allCinnamon =
     {
         'Keep original': '0.00',
         'Sugar milk': '0.00',
         'Vanilla milk': '0.50',
         'Double chocolate': '1.50',
-    }
-]
+    };
 
 let element = document.getElementById("glazing");
 for(index in allCimnamon) {

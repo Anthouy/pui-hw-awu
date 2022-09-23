3f1e5e219f19973434b401f4a2b26a417a5b718c
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-09-23 15:33:43 -0400

    runtextfruits

--- solution-hw3/pricecomp.js
+++ solution-hw3/pricecomp.js
@@ -3,19 +3,57 @@ console.log("Check1");
 let select = document.getElementById("glazing")
 
 let allCinnamon = [
-    "Keep original",
-    "Sugar milk",
-    "Vanilla milk",
-    "Double chocolate"
-];
+    {
+        "1": {
+                glazing: 'Keep original',
+                priceadapt: '0.00',
+            }
+    },
+    {
+        "2": {
+            glazing: 'Sugar milk',
+            priceadapt: '0.00',
+        }
+    },
+    {
+        "3": {
+            glazing: 'Vanilla milk',
+            priceadapt: '0.50',
+        }
+    },
+    {
+        "4": {
+            glazing: 'Double chocolate',
+            priceadapt: '1.50',
+        }
+    },
+]
 
-function glazingChange() {
-    for (let i = 0; i < allCinnamon.length; i++) {
-        let opt = allCinnamon[i];
-        let el = document.createElement("allCinnamon");
-        el.textContent = opt
-        el.value = opt;
-        select.appendChild(el);
-    }
+for (var key in fruits) {
+    var fruit = fruits[key];
+    _select.options[_select.options.length] = new Option(fruit.Description, fruit.id);
+  }
+  
+  document.body.appendChild(_select);
 
+
+  var fruits =
+{
+  "1": {
+        "id": 1,
+        "Description": "Apple",
+        "groupID": 0
+       },
+  "2": {
+        "id": 2,
+        "Description": "Peach",
+        "groupID": 0
+       }
+};
+var _select = document.createElement("select");
+for (var key in fruits) {
+  var fruit = fruits[key];
+  _select.options[_select.options.length] = new Option(fruit.Description, fruit.id);
 }
+
+document.body.appendChild(_select);

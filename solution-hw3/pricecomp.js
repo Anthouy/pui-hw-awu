5d112eba545c65531a0f2cd97f2c377a8846b1f1
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-09-23 15:01:25 -0400

    emptyhtmldropdown

--- solution-hw3/pricecomp.js
+++ solution-hw3/pricecomp.js
@@ -3,48 +3,22 @@ console.log("Check1");
 let allCinnamon = [
     {
         glazing: 'Keep original',
-        price: '2.49',
+        price: '+ $0.00',
     },
     {
         glazing: 'Sugar Milk',
-        price: '2.49',
+        price: '+ $0.00',
     },
     {
         glazing: 'Vanilla milk',
-        price: '2.99',
+        price: '+ $0.50',
     },
     {
         glazing: 'Double Chocolate',
-        price: '3.99',
+        price: '+ $1.50',
     },
 ];
-/**
- * 
- * @param cinnamon
- */
 
-
-function displayCinnamon(cinnamon) {
-    console.log("Running update")
-    let cinnamonTitleElement = document.querySelector('');
-    let cinnamonInfoElement = document.querySelector('#totalcost');
-
-    cinnamonTitleElement.innerText = cinnamon.glazing;
-    cinnamonInfoElement.innetText = cinnamon.price;
+function glazingChange() {
+    allCinnamon.push(newCinnamon);
 }
-
-function onSelectValueChange() {
-    console.log('You selected' + this.value);
-
-    let cinnamonIndex = parseInt(this.value);
-
-    let cinnamonToDisplay = allCinnamon[cinnamonIndex];
-
-    displayCinnamon(cinnamonToDisplay);
-}
-
-let selectElement = document.querySelector('#glazing')
-
-selectElement.addEventListener('change', onSelectValueChange);
-
-displayCinnamon(allCinnamon[0]);

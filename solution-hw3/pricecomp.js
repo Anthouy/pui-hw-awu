19b15ebbf079396cdd6dc0d37cb27bd2efb232f0
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-09-23 16:34:19 -0400

    ????glazetypecall?

--- solution-hw3/pricecomp.js
+++ solution-hw3/pricecomp.js
@@ -1,14 +1,14 @@
 console.log("Check1");
 
 let allCinnamon = [];
-allCinnamon[0] = 0, 'Keep original';
-allCinnamon[1] = 0, 'Sugar milk';
-allCinnamon[2] = 0.50, 'Vanilla milk';
-allCinnamon[3] = 1.50, 'Double chocolate';
+allCinnamon[0] = {priceadapt: 0, glazetype: 'Keep original'};
+allCinnamon[1] = {priceadapt: 0, glazetype: 'Sugar milk'};
+allCinnamon[2] = {priceadapt: 0.50, glazetype: 'Vanilla milk'};
+allCinnamon[3] = {priceadapt: 1.50, glazetype: 'Double chocolate'};
 
 let selectGlaze = document.getElementById("glazing")
 for(index in allCinnamon) {
-    selectGlaze.options[selectGlaze.options.length] = new Option(allCinnamon[index], index, index);
+    selectGlaze.options[selectGlaze.options.length] = new Option(allCinnamon[index].glazetype);
 }
 
 let allSize =

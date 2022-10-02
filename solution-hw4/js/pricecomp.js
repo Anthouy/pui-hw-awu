ac8e53483055e32860eba6bbf5f69a9a173ec13a
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-02 15:11:41 -0400

    movedupdateDetailabovepricecalc

--- solution-hw4/js/pricecomp.js
+++ solution-hw4/js/pricecomp.js
@@ -1,3 +1,53 @@
+console.log("rollsData Load Check");
+
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+let cart = [];
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll');
+
+function testing() {
+    console.log(queryString)
+    console.log(params)
+    console.log(rollType)
+    let baserollsprice = rolls[rollType].basePrice
+    console.log(rollsprice)
+}
+
+function updateDetail() {
+    let detailTitle = document.querySelector(".subtext").innerHTML = rollType + " Cinnamon Roll"
+    console.log("Detail Title Update")
+    let detailImage = document.querySelector(".cinnamondetail").src = "assets/products/" + rollType + "-cinnamon-roll.jpg";
+    console.log("Detail Image Update")
+}
+
 console.log("Load Check");
 
 // Array of cinnamon glazing options and price adjustments.
@@ -27,33 +77,6 @@ for(index in allSize) {
     selectSize.options[selectSize.options.length] = new Option(allSize[index].cinnCount);
     console.log("Populating Pack Size Menu")
 }
-
-const rolls = {
-    "Original": {
-        "basePrice": 2.49,
-        "imageFile": "original-cinnamon-roll.jpg"
-    },
-    "Apple": {
-        "basePrice": 3.49,
-        "imageFile": "apple-cinnamon-roll.jpg"
-    },
-    "Raisin": {
-        "basePrice": 2.99,
-        "imageFile": "raisin-cinnamon-roll.jpg"
-    },
-    "Walnut": {
-        "basePrice": 3.49,
-        "imageFile": "walnut-cinnamon-roll.jpg"
-    },
-    "Double-Chocolate": {
-        "basePrice": 3.99,
-        "imageFile": "double-chocolate-cinnamon-roll.jpg"
-    },
-    "Strawberry": {
-        "basePrice": 3.99,
-        "imageFile": "strawberry-cinnamon-roll.jpg"
-    }    
-};
 
 // Initial global variables for price calculation.
 let glazing = 0;
@@ -127,27 +150,4 @@ function calculatePrice() {
     y = x * amount;
     document.getElementById("totalcost").innerHTML = "$" + y.toFixed(2);
     console.log(y)
-}
-
-console.log("rollsData Load Check");
-
-let cart = [];
-
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get('roll');
-
-function testing() {
-    console.log(queryString)
-    console.log(params)
-    console.log(rollType)
-    let baserollsprice = rolls[rollType].basePrice
-    console.log(rollsprice)
-}
-
-function updateDetail() {
-    let detailTitle = document.querySelector(".subtext").innerHTML = rollType + " Cinnamon Roll"
-    console.log("Detail Title Update")
-    let detailImage = document.querySelector(".cinnamondetail").src = "assets/products/" + rollType + "-cinnamon-roll.jpg";
-    console.log("Detail Image Update")
 }

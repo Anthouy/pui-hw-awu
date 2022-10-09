eaf5ef56ca0f949d39d2facd4785eacb52aaa6ff
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-08 20:12:39 -0400

    cartcalcfix?

--- solution-hw5/js/app.js
+++ solution-hw5/js/app.js
@@ -230,7 +230,7 @@ function calculatePrice() {
 
 // Calculation function for cart.
 function calculatecartPrice() {
-    costList = document.querySelectorAll(itemprice)
+    costList = document.querySelectorAll(".itemprice")
     console.log(costList)
 
     x = baserollsprice + glazing;

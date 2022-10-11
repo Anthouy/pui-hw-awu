b58a6da03bfad2a5fb7d5dc133fd39dc81c85381
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-11 15:53:53 -0400

    queryselector

--- solution-hw5/js/app.js
+++ solution-hw5/js/app.js
@@ -56,6 +56,7 @@ function createItem() {
             const clone = template.content.cloneNode(true);
             document.querySelector('main').prepend(clone);
             console.log("Items Created");
+            document.querySelector("cartdescriptionsmall");
             document.getElementsByClassName("cartdescriptionsmall").innerHTML = "test";
         }
     }

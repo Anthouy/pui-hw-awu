bd7b2af76beec248fd63aaeedc7ce04be8b13770
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-12 15:57:32 -0400

    ????

--- solution-hw5/js/itemapp.js
+++ solution-hw5/js/itemapp.js
@@ -83,6 +83,7 @@ function addNewRoll(rollType, rollGlazing, packSize, basePrice) {
     const roll = new Roll(rollType, rollGlazing, packSize, basePrice);
     rollSet.add(roll);
     return roll;
+    console.log(rollSet)
 }
 
 // takes cart html and duplicates the template

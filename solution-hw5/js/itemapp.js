5cbd9e8715ccd1a094b563d9b13407ecb3bc2b4b
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-12 15:42:06 -0400

    asgawfaw

--- solution-hw5/js/itemapp.js
+++ solution-hw5/js/itemapp.js
@@ -1,3 +1,68 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpeg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpeg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpeg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpeg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpeg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpeg"
+    }    
+};
+
+let allGlazing = [
+    {
+      glazing: 'Keep Original',
+      price: 0.00,
+    },
+    {
+      glazing: 'Sugar Milk',
+      price: 0.00,
+    },
+    {
+      glazing: 'Vanilla Milk',
+      price: 0.50,
+    },
+    {
+      glazing: 'Double Chocolate',
+      price: 1.50,
+    },
+  ];
+
+  let allPackSize = [
+    {
+      packSize: 1,
+      priceAdaptation: 1,
+    },
+    {
+      packSize: 3,
+      priceAdaptation: 3,
+    },
+    {
+      packSize: 6,
+      priceAdaptation: 5,
+    },
+    {
+      packSize: 12,
+      priceAdaptation: 10,
+    }
+  ];
+
 class Roll { 
     // creates a roll class
     constructor(rollType, rollGlazing, packSize, basePrice) {

7c84b47b8a0b4148c95acf7f1350c6d69a44aeca
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-11 23:31:02 -0400

    asgwgaawg

--- solution-hw5/js/app.js
+++ solution-hw5/js/app.js
@@ -62,7 +62,7 @@ function createItem() {
 function removeItem() {
     console.log('Remove')
     let item = document.querySelector('#item-template');
-    item.content.querySelector('.topsection').remove();
+    item.content.querySelector('.topsection').innerHTML = ''
 }
 
 const queryString = window.location.search;

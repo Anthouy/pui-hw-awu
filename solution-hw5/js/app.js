958ec748f2339bc0ed64331a8bb2f915a339210a
Anthouy <89866184+Anthouy@users.noreply.github.com>
2022-10-12 12:43:19 -0400

    ???

--- solution-hw5/js/app.js
+++ solution-hw5/js/app.js
@@ -1,7 +1,5 @@
 console.log("rollsData Load Check");
 
-let calculatedprice = 0;
-
 const rolls = {
     "Original": {
         "basePrice": 2.49,
@@ -29,8 +27,6 @@ const rolls = {
     }    
 };
 
-let cart = [];
-
 class Roll {
     constructor(rollType, rollGlazing, packSize, basePrice) {
         this.type = rollType;
@@ -40,62 +36,22 @@ class Roll {
     }
 }
 
-function createItem() {
-    let x = 0
-    popCart()
-        for(index in cart) {
-            let item = document.querySelector('#item-template');
-            item.content.querySelector('.cinnamoncart').src = "assets/products/" + rolls[cart[x].type].imageFile
-            item.content.querySelector('.cartdescriptionsmall').innerHTML = cart[x].type + ' Cinnamon Roll<br>Glazing: ' + cart[x].glazing + '<br>Pack Size: ' + cart[x].size + '<br>'
-            item.content.querySelector('.itemprice').innerHTML = '<br>$ ' + cart[x].basePrice.toFixed(2)
-            const template = document.querySelector('#item-template');
-            const clone = template.content.cloneNode(true);
-            document.querySelector('main').prepend(clone);
-            console.log("Items Created");
-            x++
-        }
-        y = cart[0].basePrice + cart[1].basePrice + cart[2].basePrice + cart[3].basePrice
-        document.querySelector('.carttotal').innerHTML = '$ ' + y.toFixed(2)
-        console.log(y.toFixed(2))
-    }
+const itemSet = new Set();
 
-function removeItem() {
-    console.log('Remove')
-    let child = document.querySelectorAll('.remove')
-    if (child = document.querySelectorAll('.remove')[1]) {
-        const element = document.querySelectorAll('#itemcard')[1];
-        element.remove();
-        cart.splice(0, 1)
-        y = cart[0].basePrice + cart[1].basePrice + cart[2].basePrice + cart[3].basePrice
-        document.querySelector('.carttotal').innerHTML = '$ ' + y.toFixed(2)
-        console.log(y.toFixed(2))
-    }
-    else if (child = document.querySelectorAll('.remove')[2]) {
-        const element = document.querySelectorAll('#itemcard')[2];
-        element.remove();
-        cart.splice(1, 1)
-        y = cart[0].basePrice + cart[1].basePrice + cart[2].basePrice + cart[3].basePrice
-        document.querySelector('.carttotal').innerHTML = '$ ' + y.toFixed(2)
-        console.log(y.toFixed(2))
-    }
-    else if (child = document.querySelectorAll('.remove')[3]) {
-        const element = document.querySelectorAll('#itemcard')[3];
-        element.remove();
-        cart.splice(2, 1)
-        y = cart[0].basePrice + cart[1].basePrice + cart[2].basePrice + cart[3].basePrice
-        document.querySelector('.carttotal').innerHTML = '$ ' + y.toFixed(2)
-        console.log(y.toFixed(2))
-    }
-    else if (child = document.querySelectorAll('.remove')[4]) {
-        const element = document.querySelectorAll('#itemcard')[4];
-        element.remove();
-        cart.splice(3, 1)
-        y = cart[0].basePrice + cart[1].basePrice + cart[2].basePrice + cart[3].basePrice
-        document.querySelector('.carttotal').innerHTML = '$ ' + y.toFixed(2)
-        console.log(y.toFixed(2))
-    }
-    }
-    
+function addNewItem(rollType, rollGlazing, packSize, basePrice) {
+    const item = new Roll(rollType, rollGlazing, packSize, basePrice);
+    itemSet.add(item);
+    return item;
+}
+
+function createItem(item) {
+    const template = document.querySelector('#item-template')
+    const clone = template.content.cloneNode(true);
+
+    item.element = clone.querySelector('.topsection');
+}
+
+let cart = [];
 
 const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
@@ -157,7 +113,6 @@ let rollGlazing = "";
 let packSize = "";
 let glazing = 0;
 let amount = 1;
-
 let baserollsprice = rolls[rollType].basePrice;
 let rollimg = rolls[rollType].imageFile
 document.getElementById("glazing").addEventListener("change", calculatePrice);
@@ -230,7 +185,7 @@ function packChange() {
         }
 }
 
-// Calculation function for detail.
+// Calculation function.
 function calculatePrice() {
     x = baserollsprice + glazing;
     y = x * amount;
@@ -238,33 +193,8 @@ function calculatePrice() {
     console.log(y)
 }
 
-// Calculation function for cart.
-function calculatecartPrice() {
-
-}
-
 function addtoCart() {
     let cinnamonRoll = new Roll(rollType, rollGlazing, packSize, baserollsprice)
     cart.push(cinnamonRoll)
     console.log(cinnamonRoll)
 }
-
-function popCart() {
-    let cartitem1 = new Roll('Apple', 'Original', 3, 10.47)
-    let cartitem2 = new Roll('Raisin', 'Sugar milk', 3, 8.97)
-    let cartitem3 = new Roll('Walnut', 'Vanilla milk', 12, 39.90)
-    let cartitem4 = new Roll('Original', 'Sugar milk', 1, 2.49)
-    cart.push(cartitem1, cartitem2, cartitem3, cartitem4)
-    console.log(cart)
-}
-
-// function popCart() {
-//     let cartRolls = {
-//     roll1 : {rollType: 'Original', rollGlazing: 'Sugar milk', packSize: '1', calculatedprice: 2.49},
-//     roll2 : {rollType: 'Walnut', rollGlazing: 'Vanilla milk', packSize: '12', calculatedprice: 39.90},
-//     roll3 : {rollType: 'Raisin', rollGlazing: 'Sugar milk', packSize: '3', calculatedprice: 8.97},
-//     roll4 : {rollType: 'Apple', rollGlazing: 'Original', packSize: '3', calculatedprice: 10.47},
-// }
-//     cart.push(cartRolls)
-//     console.log(cart)
-// }
